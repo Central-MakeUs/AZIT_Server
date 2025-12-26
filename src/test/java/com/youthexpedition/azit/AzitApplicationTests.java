a6a5c1dfe6516caf7b51bc8b5874577badfb6a2d
hikhjin <hikhjin@gmail.com>
2025-12-27 00:05:24 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/test/java/com/youthexpedition/azit/AzitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youthexpedition.azit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
