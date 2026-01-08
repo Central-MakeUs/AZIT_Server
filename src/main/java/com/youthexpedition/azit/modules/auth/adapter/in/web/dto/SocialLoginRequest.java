ec414db964a785fb7a52a38c1acca52da89933ff
hikhjin <hikhjin@gmail.com>
2026-01-08 23:11:24 +0900

    docs: 카카오 로그인 스웨거 작성

--- src/main/java/com/youthexpedition/azit/modules/auth/adapter/in/web/dto/SocialLoginRequest.java
+++ src/main/java/com/youthexpedition/azit/modules/auth/adapter/in/web/dto/SocialLoginRequest.java
@@ -2,9 +2,11 @@ package com.youthexpedition.azit.modules.auth.adapter.in.web.dto;
 
 import com.youthexpedition.azit.modules.auth.application.port.in.command.SocialLoginCommand;
 import com.youthexpedition.azit.modules.member.domain.model.enums.SocialProvider;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
 public record SocialLoginRequest (
+        @Schema(description = "소셜 서비스로부터 발급받은 인가 코드")
         @NotBlank(message = "인가코드는 필수입니다.")
         String authorizationCode
 ){
