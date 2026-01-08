f7eff4d49e59ed4fa72f59787fd319c785983c97
hikhjin <hikhjin@gmail.com>
2026-01-08 23:35:27 +0900

    fix: 누락 필드 추가

--- src/main/java/com/youthexpedition/azit/modules/member/adapter/out/persistence/MemberMapper.java
+++ src/main/java/com/youthexpedition/azit/modules/member/adapter/out/persistence/MemberMapper.java
@@ -19,6 +19,8 @@ public class MemberMapper {
                 .status(entity.getStatus())
                 .role(entity.getRole())
                 .totalPoints(entity.getTotalPoints())
+                .createdAt(entity.getCreatedAt())
+                .updatedAt(entity.getUpdatedAt())
                 .build();
     }
 
