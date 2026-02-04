f4a7ee7c227ffa2d29b7486c5314fe0bbce4a394
hikhjin <hikhjin@gmail.com>
2026-02-04 15:21:59 +0900

    refactor: dto 명세 작성 및 수정

--- src/main/java/com/youthexpedition/azit/modules/member/application/port/in/dto/AddressResponse.java
+++ src/main/java/com/youthexpedition/azit/modules/member/application/port/in/dto/AddressResponse.java
@@ -1,14 +1,23 @@
 package com.youthexpedition.azit.modules.member.application.port.in.dto;
 
 import com.youthexpedition.azit.modules.member.domain.model.Address;
+import io.swagger.v3.oas.annotations.media.Schema;
 
+@Schema(description = "배송지 응답 정보")
 public record AddressResponse(
+        @Schema(description = "배송지 ID")
         Long id,
+        @Schema(description = "수령인 성함")
         String recipientName,
+        @Schema(description = "수령인 연락처")
         String phoneNumber,
+        @Schema(description = "우편번호")
         String zipcode,
+        @Schema(description = "기본 주소")
         String baseAddress,
+        @Schema(description = "상세 주소")
         String detailAddress,
+        @Schema(description = "기본 배송지 여부")
         boolean isDefault
 ) {
     public static AddressResponse from(Address address) {
