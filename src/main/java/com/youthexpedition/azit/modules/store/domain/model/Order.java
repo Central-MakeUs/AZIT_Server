2dd10cc975549200ae8ad929dbd7f916280c9d34
hikhjin <hikhjin@gmail.com>
2026-02-12 08:35:24 +0900

    feat: 무통장입금 입금자명 컬럼 및 필드 추가

--- src/main/java/com/youthexpedition/azit/modules/store/domain/model/Order.java
+++ src/main/java/com/youthexpedition/azit/modules/store/domain/model/Order.java
@@ -26,6 +26,7 @@ public class Order {
     private final long usedPoints;
     private final long totalPaymentPrice;
     private final PaymentMethod paymentMethod;
+    private final String depositorName;
     private final String courier; // 택배사
     private final String trackingNumber; // 운송장 번호
     private OrderStatus status;
@@ -34,7 +35,7 @@ public class Order {
     private LocalDateTime updatedAt;
 
     public static Order create(Long memberId, String orderNumber, OrderAddress address, String shippingInstruction,
-                               long totalShippingFee, long usedPoints, PaymentMethod paymentMethod, List<OrderItem> orderItems) {
+                               long totalShippingFee, long usedPoints, PaymentMethod paymentMethod, String depositorName, List<OrderItem> orderItems) {
 
         long totalProductPrice = orderItems.stream()
                 .mapToLong(OrderItem::getTotalBasePrice)
@@ -58,6 +59,7 @@ public class Order {
                 .usedPoints(usedPoints)
                 .totalPaymentPrice(totalPaymentPrice)
                 .paymentMethod(paymentMethod)
+                .depositorName(depositorName)
                 .status(OrderStatus.PENDING)
                 .orderItems(orderItems)
                 .build();
