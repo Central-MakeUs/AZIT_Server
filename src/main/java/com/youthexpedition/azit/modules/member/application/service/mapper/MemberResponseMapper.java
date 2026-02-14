72f09934ded15b5d90d1a113c0be9991866bcf22
hikhjin <hikhjin@gmail.com>
2026-02-14 20:38:48 +0900

    feat: 멤버일 경우 초대 코드 null 처리

--- src/main/java/com/youthexpedition/azit/modules/member/application/service/mapper/MemberResponseMapper.java
+++ src/main/java/com/youthexpedition/azit/modules/member/application/service/mapper/MemberResponseMapper.java
@@ -3,6 +3,7 @@ package com.youthexpedition.azit.modules.member.application.service.mapper;
 import com.youthexpedition.azit.infrastructure.common.util.ImageUrlFormatUtil;
 import com.youthexpedition.azit.modules.crew.domain.model.Crew;
 import com.youthexpedition.azit.modules.crew.domain.model.CrewMember;
+import com.youthexpedition.azit.modules.crew.domain.model.enums.CrewMemberRole;
 import com.youthexpedition.azit.modules.member.application.port.in.dto.MyInfoResponse;
 import com.youthexpedition.azit.modules.member.domain.model.Member;
 import lombok.RequiredArgsConstructor;
@@ -15,12 +16,14 @@ public class MemberResponseMapper {
     private final ImageUrlFormatUtil imageUrlFormatUtil;
 
     public MyInfoResponse toMyPageResponse(Member member, CrewMember crewMember, Crew crew) {
+        String invitationCode = (crewMember.getRole() == CrewMemberRole.MEMBER) ? null : crew.getInvitationCode();
+
         return MyInfoResponse.of(
                 member.getId(),
                 member.getNickname(),
                 crewMember.getCrewId(),
                 crew.getName(),
-                crew.getInvitationCode(),
+                invitationCode,
                 imageUrlFormatUtil.buildFullImageUrl(crew.getImageUrl()),
                 crewMember.getRole(),
                 imageUrlFormatUtil.buildFullImageUrl(member.getProfileImageUrl()),
