19d0ba3fae213f01f9da4a24c15b19ec3affc875
hikhjin <hikhjin@gmail.com>
2026-02-19 00:56:05 +0900

    refactor: 누락 final 추가

--- src/main/java/com/youthexpedition/azit/modules/crew/domain/model/Location.java
+++ src/main/java/com/youthexpedition/azit/modules/crew/domain/model/Location.java
@@ -10,7 +10,7 @@ import lombok.Getter;
 public class Location {
     private final String name;      // 장소 명칭
     private final String address;   // 상세 주소
-    private String detailedLocation; // 세부 장소
+    private final String detailedLocation; // 세부 장소
     private final Double latitude;  // 위도
     private final Double longitude; // 경도
 }
