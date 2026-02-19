4874fbd7c1d2ebf7af539892a709ba63b14f5171
hikhjin <hikhjin@gmail.com>
2026-02-19 09:58:36 +0900

    docs: 크루 일정 수정 api 스웨거 명세 작성

--- src/main/java/com/youthexpedition/azit/modules/crew/adapter/in/web/docs/CrewScheduleControllerDocs.java
+++ src/main/java/com/youthexpedition/azit/modules/crew/adapter/in/web/docs/CrewScheduleControllerDocs.java
@@ -4,6 +4,7 @@ import com.youthexpedition.azit.infrastructure.common.annotation.CurrentMemberId
 import com.youthexpedition.azit.infrastructure.common.response.CommonResponse;
 import com.youthexpedition.azit.infrastructure.config.swagger.ApiErrorCodeExamples;
 import com.youthexpedition.azit.modules.crew.adapter.in.web.dto.CreateScheduleRequest;
+import com.youthexpedition.azit.modules.crew.adapter.in.web.dto.UpdateScheduleRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -24,12 +25,12 @@ public interface CrewScheduleControllerDocs {
             * 준비물(supplies): 문자열 리스트 형식으로 전달 <br><br>
             
             **[참고 사항]** <br>
-            * 해당 크루의 정회원(JOINED 상태)만 일정을 생성할 수 있습니다. (NOT_A_CREW_MEMBER)
+            * 해당 크루의 정회원(JOINED)만 일정을 생성할 수 있습니다. (NOT_A_CREW_MEMBER)
             * 정기런은 크루의 리더만 생성 가능합니다. (ONLY_LEADER_CAN_CREATE_REGULAR_RUN)
             * 일정 제목: 최대 15자 이내로 작성해야 합니다. (INVALID_INPUT_VALUE)
             * 세부 장소: 최대 15자 이내로 작성해야 합니다. (INVALID_INPUT_VALUE)
             * 준비물: 최대 5개까지 등록 가능하며, 각 항목은 15자 이내여야 합니다. (INVALID_INPUT_VALUE)
-            * 모임 시간: 현재 시간보다 과거의 일정은 생성할 수 없습니다. (INVALID_SCHEDULE_TIME)
+            * 모임 시간: 현재 시간보다 과거의 시간으로 생성할 수 없습니다. (INVALID_SCHEDULE_TIME)
             """
     )
     @ApiErrorCodeExamples({
@@ -38,4 +39,28 @@ public interface CrewScheduleControllerDocs {
     })
     CommonResponse<Void> createSchedule(
             @PathVariable Long crewId, @CurrentMemberId Long memberId, @Valid @RequestBody CreateScheduleRequest request);
+
+    @Operation(
+            summary = "크루 일정 수정",
+            description = """
+            기존에 생성된 크루 일정 정보를 수정합니다. <br>
+            준비물 리스트의 경우, 기존 리스트를 모두 대체하는 방식으로 업데이트됩니다. <br><br>
+            
+            **[제약 사항]** <br>
+            * 해당 일정을 생성한 본인만 수정할 수 있습니다. (FORBIDDEN_ERROR)
+            * 해당 크루의 정회원(JOINED)이어야 합니다. (NOT_A_CREW_MEMBER)
+            * 정기런으로 수정하거나 정기런을 수정할 경우, 반드시 리더 권한이 있어야 합니다. (ONLY_LEADER_CAN_CREATE_REGULAR_RUN)
+            * 존재하지 않는 일정 ID를 입력할 경우 수정이 불가합니다. (SCHEDULE_NOT_FOUND)
+            * 일정 제목: 최대 15자 이내로 작성해야 합니다. (INVALID_INPUT_VALUE)
+            * 세부 장소: 최대 15자 이내로 작성해야 합니다. (INVALID_INPUT_VALUE)
+            * 준비물: 최대 5개까지 등록 가능하며, 각 항목은 15자 이내여야 합니다. (INVALID_INPUT_VALUE)
+            * 모임 시간: 현재 시간보다 과거의 시간으로 수정할 수 없습니다. (INVALID_SCHEDULE_TIME) <br><br>
+            """
+    )
+    @ApiErrorCodeExamples({
+            "SCHEDULE_NOT_FOUND", "FORBIDDEN_ERROR", "NOT_A_CREW_MEMBER", "ONLY_LEADER_CAN_CREATE_REGULAR_RUN", "INVALID_SCHEDULE_TIME",
+            "UNAUTHORIZED", "EXPIRED_TOKEN", "INVALID_TOKEN", "TOKEN_REUSE_DETECTED", "BLACKLISTED_TOKEN"
+    })
+    CommonResponse<Void> updateSchedule(
+            @PathVariable Long crewId, @PathVariable Long scheduleId, @CurrentMemberId Long memberId, @Valid @RequestBody UpdateScheduleRequest request);
 }
