e90c932457770c9a51f7cba7d54af1d72fc9676a
hikhjin <hikhjin@gmail.com>
2026-02-19 14:19:17 +0900

    refactor: 파라미터 순서 수정

--- src/main/java/com/youthexpedition/azit/modules/crew/application/port/in/command/ParticipateScheduleCommand.java
+++ src/main/java/com/youthexpedition/azit/modules/crew/application/port/in/command/ParticipateScheduleCommand.java
@@ -5,7 +5,7 @@ public record ParticipateScheduleCommand(
         Long scheduleId,
         Long memberId
 ) {
-    public static ParticipateScheduleCommand of(Long scheduleId, Long crewId, Long memberId) {
-        return new ParticipateScheduleCommand(scheduleId, crewId, memberId);
+    public static ParticipateScheduleCommand of(Long crewId, Long scheduleId, Long memberId) {
+        return new ParticipateScheduleCommand(crewId, scheduleId, memberId);
     }
 }
