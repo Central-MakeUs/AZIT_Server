df26c42662d04da9a06c2ab7d950baec304d3a9a
hikhjin <hikhjin@gmail.com>
2026-02-22 19:10:42 +0900

    refactor: fetchjoin -> any로 수정하여 데이터 정합성 문제 해결

--- src/main/java/com/youthexpedition/azit/modules/crew/adapter/out/persistence/repository/CrewScheduleRepositoryImpl.java
+++ src/main/java/com/youthexpedition/azit/modules/crew/adapter/out/persistence/repository/CrewScheduleRepositoryImpl.java
@@ -66,7 +66,7 @@ public class CrewScheduleRepositoryImpl implements CrewScheduleRepositoryCustom 
         return queryFactory.selectFrom(crewScheduleEntity)
                 .join(crewScheduleEntity.members, crewScheduleMemberEntity)
                 .where(
-                        crewScheduleMemberEntity.memberId.eq(memberId),
+                        crewScheduleEntity.members.any().memberId.eq(memberId),
                         crewScheduleEntity.status.eq(ScheduleStatus.ACTIVE), // 취소된 일정 제외
                         crewScheduleEntity.meetingAt.goe(LocalDateTime.now()) // 지난 일정 제외
                 )
@@ -79,7 +79,7 @@ public class CrewScheduleRepositoryImpl implements CrewScheduleRepositoryCustom 
         return queryFactory.selectFrom(crewScheduleEntity)
                 .join(crewScheduleEntity.members, crewScheduleMemberEntity)
                 .where(
-                        crewScheduleMemberEntity.memberId.eq(memberId),
+                        crewScheduleEntity.members.any().memberId.eq(memberId),
                         crewScheduleEntity.status.eq(ScheduleStatus.ACTIVE),
                         // 오늘 00:00:00 ~ 23:59:59 사이
                         crewScheduleEntity.meetingAt.between(now.with(LocalTime.MIN), now.with(LocalTime.MAX))
@@ -94,7 +94,7 @@ public class CrewScheduleRepositoryImpl implements CrewScheduleRepositoryCustom 
                 queryFactory.selectFrom(crewScheduleEntity)
                         .join(crewScheduleEntity.members, crewScheduleMemberEntity)
                         .where(
-                                crewScheduleMemberEntity.memberId.eq(memberId),
+                                crewScheduleEntity.members.any().memberId.eq(memberId),
                                 crewScheduleEntity.status.eq(ScheduleStatus.ACTIVE),
                                 crewScheduleEntity.meetingAt.gt(now.with(LocalTime.MAX)) // 오늘 이후
                         )
@@ -106,10 +106,11 @@ public class CrewScheduleRepositoryImpl implements CrewScheduleRepositoryCustom 
     @Override
     public List<CrewScheduleEntity> findAllByCrewIdAndMemberId(Long crewId, Long memberId) {
         return queryFactory.selectFrom(crewScheduleEntity)
-                .join(crewScheduleEntity.members, crewScheduleMemberEntity).fetchJoin()
-                .where(crewScheduleEntity.crewId.eq(crewId)
-                        .and(crewScheduleMemberEntity.memberId.eq(memberId))
-                        .and(crewScheduleEntity.status.eq(ScheduleStatus.ACTIVE)))
+                .where(
+                        crewScheduleEntity.crewId.eq(crewId),
+                        crewScheduleEntity.members.any().memberId.eq(memberId),
+                        crewScheduleEntity.status.eq(ScheduleStatus.ACTIVE)
+                )
                 .fetch();
     }
 
