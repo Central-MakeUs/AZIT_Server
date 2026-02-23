821e5baba58a77e6adf13ed5bafd709a84f58809
hikhjin <hikhjin@gmail.com>
2026-02-23 21:10:55 +0900

    docs: 장소 검색 api 스웨거 명세 작성

--- src/main/java/com/youthexpedition/azit/modules/location/adapter/in/web/docs/LocationControllerDocs.java
+++ src/main/java/com/youthexpedition/azit/modules/location/adapter/in/web/docs/LocationControllerDocs.java
@@ -15,15 +15,15 @@ public interface LocationControllerDocs {
     @Operation(
             summary = "장소 검색",
             description = """
-            카카오 등 소셜 플랫폼의 인가 코드를 통해 로그인을 진행하고 JWT 토큰과 회원의 현재 상태, 최근 가입한 크루 ID를 반환합니다. <br><br>
+            네이버 지역 검색 API를 호출하여 입력한 키워드에 부합하는 장소 목록을 최대 5개 반환합니다. <br>
+            일정 등록 시 집합 장소를 검색하기 위한 용도로 사용됩니다. <br><br>
             
             **[참고 사항]** <br>
-            * 보안을 위해 리프레시 토큰은 HttpOnly 쿠키에 저장되어 발급됩니다.
-            * 응답으로 받은 **status** 값에 따라 앱의 초기 진입 화면(약관 동의, 온보딩, 메인 등)을 결정해야 합니다. <br>
+            * 도로명 주소를 우선적으로 반환하며, 도로명 주소가 없을 경우 지번 주소를 반환합니다. <br>
             """
     )
     @ApiErrorCodeExamples({
-            "INVALID_SOCIAL_CODE", "SOCIAL_AUTHENTICATION_FAILED"
+            "UNAUTHORIZED", "EXPIRED_TOKEN", "INVALID_TOKEN", "TOKEN_REUSE_DETECTED", "BLACKLISTED_TOKEN"
     })
     CommonResponse<List<LocationSearchResponse>> search(@RequestParam String query);
 
