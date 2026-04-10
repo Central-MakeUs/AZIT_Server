31891e15dd7dce2d0c07a75763a506bb320d1b25
hikhjin <hikhjin@gmail.com>
2026-04-10 16:00:23 +0900

    docs: 소셜 로그인 스웨거 명세 수정

--- src/main/java/com/youthexpedition/azit/modules/auth/adapter/in/web/docs/AuthControllerDocs.java
+++ src/main/java/com/youthexpedition/azit/modules/auth/adapter/in/web/docs/AuthControllerDocs.java
@@ -26,7 +26,7 @@ public interface AuthControllerDocs {
     @Operation(
             summary = "소셜 로그인 (애플 제외)",
             description = """
-            카카오 등 소셜 플랫폼의 인가 코드를 통해 로그인을 진행하고 JWT 토큰과 회원의 현재 상태, 최근 가입한 크루 ID를 반환합니다. <br><br>
+            카카오 등 소셜 플랫폼의 인가 코드 및 액세스 토큰을 통해 로그인을 진행하고 JWT 토큰과 회원의 현재 상태, 최근 가입한 크루 ID를 반환합니다. <br><br>
             
             **[참고 사항]** <br>
             * 보안을 위해 리프레시 토큰은 HttpOnly 쿠키에 저장되어 발급됩니다.
@@ -34,7 +34,7 @@ public interface AuthControllerDocs {
             """
     )
     @ApiErrorCodeExamples({
-            "INVALID_SOCIAL_CODE", "SOCIAL_AUTHENTICATION_FAILED"
+            "INVALID_SOCIAL_CODE", "SOCIAL_AUTHENTICATION_FAILED", "INVALID_KAKAO_ACCESS_TOKEN"
     })
     CommonResponse<SocialLoginResponse> socialLogin(
             @PathVariable SocialProvider provider, @Valid @RequestBody SocialLoginRequest request, HttpServletResponse response);
